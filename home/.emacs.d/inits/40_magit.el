5a43156d0ecfe56878e14652aaf2243d48cbc539
umakoz <um@makoz.net>
2014-04-10 11:50:39 +0900

    magit-status を C-c m に割り当てた

--- home/.emacs.d/inits/40_magit.el
+++ home/.emacs.d/inits/40_magit.el
@@ -1,3 +1,4 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
-(when (require 'magit nil t))
+(when (require 'magit nil t)
+  (define-key global-map (kbd "C-c m") 'magit-status))
