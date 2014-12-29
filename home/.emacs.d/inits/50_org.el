1ae36b8eafb9de50338462c8d985564b447a1946
umakoz <um@makoz.net>
2014-12-29 12:25:23 +0900

    configure org-mode.

--- home/.emacs.d/inits/50_org.el
+++ home/.emacs.d/inits/50_org.el
@@ -1,12 +1,35 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
-;;(add-to-list 'load-path "~/.emacs.d/elisp/org/lisp")
-;;(when (require 'org-install nil t)
-  (setq org-hide-leading-stars t
-        org-odd-levels-only t
-        org-log-done 'time)
+(setq org-startup-truncated nil
+      org-return-follows-link t
+      org-hide-leading-stars t
+      org-odd-levels-only nil
+      org-log-done 'time
+      org-directory "~/Dropbox/org/"
+      org-default-notes-file (concat org-directory "notes.org")
+      org-agenda-files (list org-directory))
 
-  (global-set-key (kbd "C-c l") 'org-store-link)
-  (global-set-key (kbd "C-c c") 'org-capture)
-  (global-set-key (kbd "C-c a") 'org-agenda)
-  (global-set-key (kbd "C-c b") 'org-iswitchb);;)
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "SOMEDAY(s)")))
+
+(setq org-todo-keyword-faces 
+      '(("TODO" :foreground "red" :weight bold)
+        ("WAIT" :foreground "yellow" :weight bold)
+        ("DONE" :foreground "green" :weight bold)
+        ("SOMEDAY" :foreground "cyan" :weight bold)))
+
+(setq org-capture-templates
+      '(("t" "Todo" entry
+         (file+headline nil "Inbox")
+         "** TODO %?\n   %i\n   %a\n   %t")
+        ("b" "Bug" entry
+         (file+headline nil "Inbox")
+         "** TODO %?   :bug:\n   %i\n   %a\n   %t")
+        ("i" "Idea" entry
+         (file+headline nil "New Ideas")
+         "** %?\n   %i\n   %a\n   %t")))
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
