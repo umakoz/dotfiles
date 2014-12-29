1c693f5ff74960bbd4c8355388dc1d943fc4de76
umakoz <um@makoz.net>
2014-12-29 12:27:02 +0900

    imploce c-mode and c++-mode.

--- home/.emacs.d/inits/50_c++.el
+++ home/.emacs.d/inits/50_c++.el
@@ -1,8 +1,8 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
 
 (add-hook 'c++-mode-hook '(lambda ()
-                            (c-set-style "stroustrup")
+                            (c-set-style "cc-mode")
                             (c-set-offset 'case-label '+)
                             (setq indent-tabs-mode nil)
-                            (define-key c++-mode-map "\C-m" 'newline-and-indent)
-                            (gtags-mode 1)))
+                            'helm-gtags-mode
+                            'ac-cc-mode-setup))
